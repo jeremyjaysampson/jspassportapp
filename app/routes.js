013e82b9ac3d56b84c79b9b6bb9335e721e6bfcf
jeremyjaysampson <jsampson@mrsam.com>
2016-07-03 13:15:48 +1000

    added home

--- app/routes.js
+++ app/routes.js
@@ -35,6 +35,35 @@ module.exports = function(app, passport) {
         res.redirect('/');
     });
 
+	// ===========JEREMY HOME===============
+	// show the home page with login form
+	app.get('/home', function(req, res) {
+
+		// render the page and pass in any flash data if it exists
+		res.render('home.ejs', { message: req.flash('loginMessage') });
+	});
+
+	// process the login form
+	app.post('/home', passport.authenticate('local-login', {
+            successRedirect : '/profile', // redirect to the secure profile section
+            failureRedirect : '/home', // redirect back to the signup page if there is an error
+            failureFlash : true // allow flash messages
+		}),
+        function(req, res) {
+            console.log("hello");
+
+            if (req.body.remember) {
+              req.session.cookie.maxAge = 1000 * 60 * 3;
+            } else {
+              req.session.cookie.expires = false;
+            }
+        res.redirect('/home');
+    });
+
+    
+	// =========END JEREMY HOME=============
+    
+    
 	// =====================================
 	// SIGNUP ==============================
 	// =====================================
